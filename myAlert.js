fcf7613719d71facca5274ce1627b8d9db421466
RunTu <1129245399@qq.com>
2022-10-26 22:10:27 +0800

    1.0

--- myAlert.js
+++ myAlert.js
@@ -133,7 +133,7 @@ function popFunction(status, text) {
             // 设置初始样式
             icon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>'
             css(icon, {
-                'border': 'solid 4px #F37878',
+                'border': 'solid 4px #EB5353',
                 'transform': 'rotate(45deg)',
                 'transition': 'all 0.3s 0.3s',
                 'display': 'flex',
@@ -145,7 +145,7 @@ function popFunction(status, text) {
                 'width': '30px',
                 'height': '30px',
                 'transition': 'all 0.3s 0.3s',
-                'fill': '#E0144C'
+                'fill': '#B20600'
             })
             // 设置最终样式
             setTimeout(() => {
@@ -220,8 +220,9 @@ function popFunction(status, text) {
                 }, 10)
             }, 300);
 
-        // 若传入参数非法，结束该函数
+        // 若传入参数非法，删除所有节点并结束该函数
         } else {
+            pop.remove()
             return null
         }
 
